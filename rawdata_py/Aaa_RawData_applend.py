d576c6c1e19d722641c3e9d91ad71dbd9f4a1916
tongshan12 <1qaz@WSX>
2017-08-25 15:03:38 +0800

    mili ; applend 取数代码;python
    通过pandas.read_sql可以直接取数，效率更好，无需一条一条写入文件

--- rawdata_py/Aaa_RawData_applend.py
+++ rawdata_py/Aaa_RawData_applend.py
@@ -49,10 +49,10 @@ def csv_applend(lend_table):
         counter += 1
 
 csv_applend("user")
-csv_applend("user_base_info")
-csv_applend("circular")
+#csv_applend("user_base_info")
+#csv_applend("circular")
 csv_applend("loan_info")
-csv_applend("user_verification_info")
+#csv_applend("user_verification_info")
 #==============================================================================
 # csv_applend("id_verification")
 # csv_applend("operator_verification")
@@ -67,3 +67,58 @@ cnx.close()
 end = time.clock()
 print('Running time: %s Seconds'%(end-start))
 
+
+
+
+#==============================================================================
+# 第二种取数的方法
+import time
+start =time.clock()
+
+import mysql.connector
+import os
+import sys
+import re
+import csv
+import pandas as pd
+
+sys.path.append(r"F:\BQS_ETL v0.2\lib")
+import dbconfig_importer
+
+db_config = r"F:\BQS_ETL v0.2\cfg\database_config.json"
+tmp_dir = r"D:\mili\Datamart\rawdata_csv_py\applend"
+
+cnx = dbconfig_importer.connect(db_config)
+
+query_cursor = cnx.cursor()
+
+def csv_applend(lend_table):
+    query="SELECT * FROM applend." + lend_table
+      
+    df = pd.read_sql(query,cnx)
+    applend_csv_file = tmp_dir + '\\'+ lend_table + ".csv"    
+    
+    df.to_csv(applend_csv_file,encoding = "utf-8" ,index = False)
+    table = ["user","loan_info"]
+   
+#==============================================================================
+#     table = [
+#             "user",
+#             "user_base_info",
+#             "circular",
+#             "loan_info",
+#             "user_verification_info",
+#             "id_verification",
+#             "operator_verification",
+#             "user_relation",
+#             "user_relation_his"]
+#==============================================================================
+    
+for lend_table in table:
+    csv_applend(lend_table)
+                 
+query_cursor.close()
+cnx.close()
+
+end = time.clock()
+print('Running time: %s Seconds'%(end-start))
